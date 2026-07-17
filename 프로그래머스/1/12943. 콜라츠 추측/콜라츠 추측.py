cb797d27b7c5f95e46710c8cee9e801089e88798
rathat1234 <93762538+rathat1234@users.noreply.github.com>
2026-07-18 00:22:22 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,20 @@
+def solution(num):
+    answer = 0
+    while True:
+        if num == 1:
+            break
+        elif answer > 500:
+            answer = -1
+            break
+        
+        if num %2 == 0:
+            num /= 2
+            answer +=1
+        else:
+            num = num*3+1
+            answer +=1
+            
+        
+            
+        
+    return answer
