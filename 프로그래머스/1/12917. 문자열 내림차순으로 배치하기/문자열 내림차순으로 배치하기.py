90c5c5f5e866cc7c05705d26746fb76bbe5eb8cc
rathat1234 <93762538+rathat1234@users.noreply.github.com>
2026-07-18 12:42:53 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 4.98 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    arr = list(s)
+    s = ""
+    for j in range(len(arr)-1,0,-1):
+        for i in range(j):
+            if arr[i] < arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+    
+    for i in range(len(arr)):
+        s += arr[i]
+    return s
