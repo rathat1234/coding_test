2cabfd864422b10b85acc1a630b29e463cb80360
JaewooNa <93762538+rathat1234@users.noreply.github.com>
2026-08-13 21:31:17 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/README.md
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 핸드폰 번호 가리기 - 12948 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12948) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12948?language=c) 
 
 ### 성능 요약
 
-메모리: 11.6 MB, 시간: 0.00 ms
+메모리: 4.95 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 18일 00:28:44
+2026년 08월 13일 21:31:26
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* phone_number) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int number_len = strlen(phone_number);
+    char* p_number = (char*)malloc(sizeof(char)*number_len+1);
+    char* answer = (char*)malloc(sizeof(char)*number_len+1);
+    strcpy(p_number,phone_number);
+    
+    for(int i = 0 ; i <= number_len;i++)
+    {
+        if(i<(number_len-4)){
+            *(answer+i) = '*';      
+        }
+        else if(number_len==i){
+            *(answer+i) = '\0';
+            
+        }
+        else{
+            *(answer+i) = *(p_number+i);
+        }
+    }
+    
+    
+    return answer;
+}
